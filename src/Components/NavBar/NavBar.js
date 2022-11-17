96bbce5e6bf0fee9f0ce47b8596a824718de02f2
Kortney <kortney@iMac.local>
2022-11-17 16:06:45 -0400

    added css to NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./NavBar.css";
 
 export default function NavBar() {
     return (
         <ul className="navLinks">
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
+            <li className="navLink">Home</li>
+            <li className="navLink">About</li>
+            <li className="navLink">Projects</li>
         </ul>
     );
 }
