25a8c0b50b254177348e22caa03e232c0c85d84d
Kortney <kortney@iMac.local>
2022-11-22 11:03:24 -0400

    added search button and formatted it. Changed colour of body

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./NavBar.css";
 import patreon from "../../images/Patreon.png";
+import search from "../../images/search.png";
 
 export default function NavBar() {
     return (
@@ -9,10 +10,14 @@ export default function NavBar() {
                 <li className="navLink">HOME </li>
                 <li className="navLink">ABOUT </li>
                 <li className="navLink">PROJECTS </li>
+                <li className="navLink">CONTACT </li>
             </ul>
-            <button>
+            <button className="patreonDonate">
                 <img className="logo" src={patreon} alt="logo" />
                 <span>DONATE</span>
+            </button>
+            <button className="searchButton">
+                <img className="searchicon" src={search} alt="search icon" />
             </button>
         </div>
     );
