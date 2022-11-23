4e47ba2d8443f8a63a0155170def0763eed08c95
Kortney <kortney@iMac.local>
2022-11-23 14:25:06 -0400

    styled ListenOn section

--- src/Components/ListenOn/ListenOn.js
+++ src/Components/ListenOn/ListenOn.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ListenOn.css";
 import podIcon from "../../images/podIcon.webp";
 import appleLogo from "../../images/appleLogo.png";
 import spotifyLogo from "../../images/spotifyLogo.png";
@@ -7,20 +8,20 @@ import googleLogo from "../../images/googleLogo.webp";
 export default function ListenOn() {
     return (
         <div className="listenOnContainer">
-            <div>
+            <h3>
                 <img className="podIcon" src={podIcon} alt="podcatcher icon" />
-                <h3>Subscribe On:</h3>
-            </div>
+                Subscribe On:
+            </h3>
 
-            <span>
+            <span className="ListenOnChannel">
                 <img className="logo" src={appleLogo} alt="apple logo" />
                 APPLE PODCASTS
             </span>
-            <span>
+            <span className="ListenOnChannel">
                 <img className="logo" src={googleLogo} alt="spotify logo" />
                 GOOGLE PODCASTS
             </span>
-            <span>
+            <span className="ListenOnChannel">
                 <img className="logo" src={spotifyLogo} alt="spotify logo" />
                 SPOTIFY
             </span>
