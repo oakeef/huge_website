796f7cbad717a95b7d874a5e3288ed8ae906f540
Kortney <kortney@iMac.local>
2022-11-23 14:43:11 -0400

    renamed css classes in ListenOn component

--- src/Components/ListenOn/ListenOn.js
+++ src/Components/ListenOn/ListenOn.js
@@ -8,20 +8,20 @@ import googleLogo from "../../images/googleLogo.webp";
 export default function ListenOn() {
     return (
         <div className="listenOnContainer">
-            <h3>
+            <h3 className="listenOnIconAndTitle">
                 <img className="podIcon" src={podIcon} alt="podcatcher icon" />
                 Subscribe On:
             </h3>
 
-            <span className="ListenOnChannel">
+            <span className="listenOnChannel">
                 <img className="logo" src={appleLogo} alt="apple logo" />
                 APPLE PODCASTS
             </span>
-            <span className="ListenOnChannel">
+            <span className="listenOnChannel">
                 <img className="logo" src={googleLogo} alt="spotify logo" />
                 GOOGLE PODCASTS
             </span>
-            <span className="ListenOnChannel">
+            <span className="listenOnChannel">
                 <img className="logo" src={spotifyLogo} alt="spotify logo" />
                 SPOTIFY
             </span>
