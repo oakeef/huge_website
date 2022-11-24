549a331207ff91f994655d9aba8510cd916b042b
Kortney <kortney@iMac.local>
2022-11-24 15:40:42 -0400

    updated fonts across app and fixed some spacing issues this caused

--- src/Components/LatestEpisodes/LatestEpisodes.js
+++ src/Components/LatestEpisodes/LatestEpisodes.js
@@ -4,16 +4,22 @@ import "./LatestEpisodes.css";
 
 export default function LatestEpisodes() {
     return (
-        <div className="latestEpContainer">
-            <div>
-                <h2> Latest Episodes</h2>
-                <span className="browseAll"> BROWSE ALL {">"} </span>
+        <>
+            <div className="latestEpContainer">
+                <div>
+                    <h2 className="latestEpHeader"> Latest Episodes</h2>
+                    <span className="browseAll"> BROWSE ALL {">"} </span>
+                </div>
+                <div className="latestEpButtonContainer">
+                    <button className="latestEpButton"> ← </button>
+                    <button className="latestEpButton"> → </button>
+                </div>
             </div>
-            <div className="latestEpButtonContainer">
-                <button className="latestEpButton"> → </button>
-                <button className="latestEpButton"> ← </button>
+            <div className="episodesContainer">
+                <Episode />
+                <Episode />
+                <Episode />
             </div>
-            <Episode />
-        </div>
+        </>
     );
 }
