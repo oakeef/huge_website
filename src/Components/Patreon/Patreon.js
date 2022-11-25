49f318e4b4d353528c0e0f5286cdf622b2f417df
Kortney <kortney@iMac.local>
2022-11-25 14:18:06 -0400

    added css styling to patreon componment

---
+++ src/Components/Patreon/Patreon.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Patreon.css";
+import patreonImg from "../../images/PatreonLogoName.png";
+import patreon from "../../images/Patreon.png";
+
+export default function Patreon() {
+    return (
+        <div className="patreonContainer">
+            <img
+                className="patreonImg"
+                src={patreonImg}
+                alt="patreon logo and name"
+            />
+            <div className="patreonTextContainer">
+                <h1 className="patreonHeading">Support the Show on Patreon</h1>
+                <span className="patreonText">
+                    If you want to support the show, enjoy an extra warm feeling
+                    inside when you listen, knowing you’ve helped to keep the
+                    project going.
+                </span>
+
+                <button className="patreonButton">
+                    <img className="logo" src={patreon} alt="logo" />
+                    <span>DONATE</span>
+                </button>
+            </div>
+        </div>
+    );
+}
