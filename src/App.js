2ffaaf04752d071199af81d05d19f27457a1d87c
Kortney <kortney@iMac.local>
2022-11-29 11:58:24 -0400

    added about page and route to that page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
 import Footer from "./Components/Footer/Footer";
 
 export default function App() {
     return (
         <>
             <BrowserRouter>
+                <Header />
                 <Routes>
-                    <Route path="/" element={<Header />} />
-
-                    <Route index element={<Home />} />
+                    <Route path="Home" index element={<Home />} />
+                    <Route path="About" element={<About />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./NavBar.css";
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import patreon from "../../images/Patreon.png";
 import search from "../../images/search.png";
 
@@ -7,10 +9,18 @@ export default function NavBar() {
     return (
         <div className="navContainer">
             <ul className="navLinks">
-                <li className="navLink">HOME</li>
-                <li className="navLink">ABOUT </li>
-                <li className="navLink">PROJECTS </li>
-                <li className="navLink">CONTACT </li>
+                <li className="navLink">
+                    <Link to="/Home"> Home </Link>
+                </li>
+                <li className="navLink">
+                    <Link to="/About"> About </Link>
+                </li>
+                <li className="navLink">
+                    <Link to="/Projects"> Projects </Link>
+                </li>
+                <a href="mailto:hugecrew@hoppedupeast.com">
+                    <li className="navLink">Contact </li>
+                </a>
             </ul>
             <a href="https://www.patreon.com/hoppedupeast">
                 <button className="patreonDonate">
@@ -25,6 +35,7 @@ export default function NavBar() {
             <button className="searchButton">
                 <img className="searchicon" src={search} alt="search icon" />
             </button>
+            <Outlet />
         </div>
     );
 }
