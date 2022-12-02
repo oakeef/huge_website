83c8fd22cda12099ce57e2845da5923d8e9bbed7
Kortney <kortney@iMac.local>
2022-12-02 14:40:23 -0400

    added links to quick links section in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,24 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
     return (
-        // TODO: add a tags to the links
+        // TODO: add a tag to Episodes when that is made
         <div className="footerContainer">
             <hr className="footerHR"></hr>
             <div className="footerLinksContainer">
                 <ul className="footerLinks">
                     <li className="footerLinksHeader">Quick Links</li>
-                    <li className="footerLink">Where to Listen</li>
+                    <Link to="/About">
+                        <li className="footerLink">Where to Listen</li>
+                    </Link>
                     <li className="footerLink">Episodes</li>
-                    <li className="footerLink">Discord Server</li>
+                    <a
+                        href="https://bit.ly/hoppedupdiscord"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <li className="footerLink">Discord Server</li>
+                    </a>
                 </ul>
 
                 <ul className="footerLinks">
                     <li className="footerLinksHeader">More</li>
                     <li className="footerLink"> Projects</li>
                     <li className="footerLink">Patreon</li>
-                    <li className="footerLink">Twitter</li>
+                    <li className="footerLink">YouTube</li>
                 </ul>
 
                 <ul className="footerLinks">
