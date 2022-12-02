170e9151c6ff41498012cc6b2936719a0020f723
Kortney <kortney@iMac.local>
2022-12-02 14:12:59 -0400

    added props to host img src to programmatically add the persons name to the alt text and got rid of a console.log

--- src/Components/Host/Host.js
+++ src/Components/Host/Host.js
@@ -7,7 +7,7 @@ export default function Host(props) {
             <img
                 className="hostImg"
                 src={props.hugeHost.img}
-                alt="hostHeadshot"
+                alt={props.hugeHost.name + "Headshot"}
             />
             <h4 className="hostName">{props.hugeHost.name}</h4>
             <span className="hostBio">{props.hugeHost.bio}</span>
