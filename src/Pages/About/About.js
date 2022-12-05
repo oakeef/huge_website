34e259c6b7d333067ba624469883daf90dd9feea
Kortney <kortney@iMac.local>
2022-12-05 17:46:08 -0400

    added placeholder img to about section and restyled contents

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./About.css";
+import hugeLogo from "../../images/hugeLogo.png";
 
 export default function About() {
     let podCastData = [
@@ -37,27 +38,34 @@ export default function About() {
     });
 
     return (
-        <div>
-            <div className="aboutContainer">
-                <p>
-                    Hopped-Up Gaming: East is a beer and gaming podcast
-                    originating from Halifax Nova Scotia. It started as a bunch
-                    of guys that wanted to sit around, drink a few beers and
-                    talk about video games but has since evolved into a more
-                    full featured podcast with expertly crafted beer critique.
-                    Not really. But it’s a lot of fun.
-                </p>
-                <p>
-                    Over the last 8 years HUGE (fun acronym right?) has grown
-                    and shifted to cover more than just video games. Listen to
-                    any episode today and you’ll likely hear about board games,
-                    card games, movies, pop culture and of course we still talk
-                    about craft beer and video games.
-                </p>
-                <p>
-                    You can check us out on Apple Podcasts, Google Podcasts,
-                    Spotify and more.
-                </p>
+        //TODO: REPLACE IMAGE
+        <div className="aboutContainer">
+            <div className="aboutTextContainer">
+                <img
+                    className="aboutImage"
+                    src={hugeLogo}
+                    alt="Hopped Up Gaming: East Logo"
+                />
+                <article className="aboutText">
+                    <p>
+                        <span className="aboutTextStart">
+                            Hopped-Up Gaming: East
+                        </span>{" "}
+                        is a beer and gaming podcast originating from Halifax
+                        Nova Scotia. It started as a bunch of guys that wanted
+                        to sit around, drink a few beers and talk about video
+                        games but has since evolved into a more full featured
+                        podcast with expertly crafted beer critique. Not really.
+                        But it’s a lot of fun.
+                    </p>
+                    <p>
+                        Over the last 8 years HUGE (fun acronym right?) has
+                        grown and shifted to cover more than just video games.
+                        Listen to any episode today and you’ll likely hear about
+                        board games, card games, movies, pop culture and of
+                        course we still talk about craft beer and video games.
+                    </p>
+                </article>
             </div>
             <div className="aboutWhereToListenContainer">
                 <h2 className="aboutWhereToListenHeader"> Where to Listen </h2>
