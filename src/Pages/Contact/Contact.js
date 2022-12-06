36ca81f5c50a4b2e6b44765d8c96d3de2c1cea11
Kortney <kortney@iMac.local>
2022-12-06 15:30:29 -0400

    make skeleton for the contact form and styled component.

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,9 +1,48 @@
 import React from "react";
+import "./Contact.css";
 
 export default function Contact() {
     return (
-        <>
-            <h1>contact form will go here</h1>
-        </>
+        <div className="contactContainer">
+            <h1 className="contactHeader">Contact Us</h1>
+            <p className="contactText">
+                Have a question for the HUGE crew? We'd love to hear from you.
+                Send us a message and we'll respond as soon as we can.
+            </p>
+
+            <div className="contactFormContainer">
+                <form className="contactForm">
+                    <label className="contactFormLabel" for="name">
+                        Name:
+                    </label>
+                    <input
+                        className="contactFormInput"
+                        type="text"
+                        id="name"
+                    ></input>
+                    <label className="contactFormLabel" for="email">
+                        Email:
+                    </label>
+                    <input
+                        className="contactFormInput"
+                        type="text"
+                        id="email"
+                    ></input>
+                    <label className="contactFormLabel" for="message">
+                        Message:
+                    </label>
+                    <input
+                        className="contactFormInputMessage"
+                        type="text"
+                        id="message"
+                    ></input>
+                    <input
+                        className="contactFormSubmit"
+                        type="submit"
+                        value="Submit"
+                    ></input>
+                </form>
+            </div>
+        </div>
     );
 }
