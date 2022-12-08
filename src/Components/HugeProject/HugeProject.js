1d9360899b3822eb697498a02ef70f252fbbf017
Kortney <kortney@iMac.local>
2022-12-08 15:29:46 -0400

    added hover effects to hugeProject containers

--- src/Components/HugeProject/HugeProject.js
+++ src/Components/HugeProject/HugeProject.js
@@ -5,6 +5,7 @@ import sonic2 from "../../images/sonic2.jpeg";
 export default function HugeProject(props) {
     return (
         <div className="hugeProjectContainer">
+            <button className="hugeProjectsSeeMore">learn more</button>
             <img className="hugeProjectImg" src={sonic2} alt="placeholder" />
             <span className="hugeProjectTitle">{props.hugeProject.title}</span>
             <div className="hugeProjectDescriptionContainer">
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -5,9 +5,9 @@ import "./Projects.css";
 export default function Projects() {
     let projectsData = [
         {
-            title: "XCOMMITEE",
+            title: "X-COMMITEE",
             description:
-                "An interesting take on playing the strategy game XCOM. We have one save file and pass it around for 1 hour turns and meet up at the end of each round and talk about what happened. Basically playing XCOM by Commitee. Season 1 covers the events of XCOM Enemy Unkown and Enemy Within Expansion. Season 2 covers the events of XCOM 2, Alien Hunters and War of the Chosen Expansion. You can find both Seasons on Apple Podcasts, Google Podcasts or anywhere Podcasts are found.",
+                "An interesting take on playing the strategy game XCOM. We have one save file and pass it around for 1 hour turns and meet up at the end of each round and talk about what happened. Basically playing XCOM by Commitee. Season 1 covers the events of XCOM Enemy Unkown and Enemy Within Expansion. Season 2 covers the events of XCOM 2, Alien Hunters and War of the Chosen Expansion.",
             link: "n/a",
         },
         {
