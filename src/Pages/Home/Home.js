f7cd96df212d6bf638bf3f0567ebe5ad999eb6f7
Kortney <kortney@iMac.local>
2022-12-09 15:09:24 -0400

    imported axios and react-xml-parser and then did a get from rss feed. Transformed the data into an array of objects with specifc keys that are to be displayed on the website

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "../../Components/Hero/Hero";
 import ListenOn from "../../Components/ListenOn/ListenOn";
 import LatestEpisodes from "../../Components/LatestEpisodes/LatestEpisodes";
 import HostsSection from "../../Components/HostsSection/HostsSection";
 import Patreon from "../../Components/Patreon/Patreon";
+import axios from "axios";
+
+var XMLParser = require("react-xml-parser");
+
+function stringChoppy(mystring) {
+    let slicedString = mystring.slice(0, mystring.length - 2);
+
+    return slicedString;
+}
 
 export default function Home() {
+    const [podcastEpisodes, setPodcastEpisodes] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://feeds.libsyn.com/447303/rss", {
+                "Content-Type": "application/xml; charset=utf-8",
+            })
+            .then((response) => {
+                let episodes = [];
+                let xml = new XMLParser().parseFromString(response.data);
+                xml.children[0].children.forEach((episode, index) => {
+                    if (index >= 22) {
+                        let parsedEpisode = {
+                            title: episode.children[0].value,
+                            subtitle: stringChoppy(episode.children[12].value),
+                            image: episode.children[5].attributes.href,
+                            link: stringChoppy(episode.children[4].value),
+                        };
+                        episodes.push(parsedEpisode);
+                    }
+                });
+                console.log(episodes);
+            });
+    }, []);
+
     return (
         <>
             <Hero />
