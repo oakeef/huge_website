3a0ee2e7ffc2ed385320668726ddedabec3b2839
Kortney <kortney@iMac.local>
2022-12-12 16:54:16 -0400

    installed react-simply-carousel and put it into Latest Episode to replace old rendering of episode container. Modified settings for react-simply-carousel to be styled for website

--- src/Components/LatestEpisodes/LatestEpisodes.js
+++ src/Components/LatestEpisodes/LatestEpisodes.js
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Episode from "../Episode/Episode";
+import ReactSimplyCarousel from "react-simply-carousel";
 import "./LatestEpisodes.css";
 
 export default function LatestEpisodes(props) {
     const { episodes } = props;
+
+    const [activeSlideIndex, setActiveSlideIndex] = useState(0);
 
     let latestEpisodes = [];
 
     for (let i = 0; i <= 5; i++) {
         latestEpisodes.push(<Episode key={i} episode={episodes[i]} />);
     }
+
     return (
         <>
-            <div className="latestEpContainer">
+            <div className="latestEpContainer glider">
                 <div>
                     <h2 className="latestEpHeader"> Latest Episodes</h2>
                     <span className="browseAll"> BROWSE ALL {">"} </span>
                 </div>
-                <div className="latestEpButtonContainer">
-                    <button className="latestEpButton"> ← </button>
-                    <button className="latestEpButton"> → </button>
-                </div>
             </div>
-            <div className="episodesContainer">{latestEpisodes}</div>
+
+            <ReactSimplyCarousel
+                className="episodesContainer"
+                activeSlideIndex={activeSlideIndex}
+                onRequestChange={setActiveSlideIndex}
+                itemsToShow={3}
+                itemsToScroll={1}
+                infinite={false}
+                speed={60}
+                backwardBtnProps={{
+                    className: "latestEpButton",
+                    children: "←",
+                }}
+                forwardBtnProps={{
+                    className: "latestEpButton",
+                    children: "→",
+                }}
+            >
+                {latestEpisodes}
+            </ReactSimplyCarousel>
         </>
     );
 }
