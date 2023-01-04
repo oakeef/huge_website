dc944ae346ca26310091274eafa5da33868c8f03
Kortney <kortney@iMac.local>
2023-01-04 10:45:39 -0400

    added overview to XCOM page

--- src/Pages/Xcommittee/Xcommittee.js
+++ src/Pages/Xcommittee/Xcommittee.js
@@ -4,6 +4,18 @@ export default function Xcommittee() {
     return (
         <div className="xcomContainer">
             <h2>X-Committee</h2>
+            <span>
+                An interesting take on playing the strategy game XCOM. We have
+                one save file and pass it around for 1 hour turns and meet up at
+                the end of each round and talk about what happened. Basically
+                playing XCOM by Commitee.
+            </span>
+            <span>
+                Season 1 covers the events of XCOM Enemy Unkown and Enemy Within
+                Expansion. Season 2 covers the events of XCOM 2, Alien Hunters
+                and War of the Chosen Expansion. You can find both Seasons on
+                Apple Podcasts, Google Podcasts or anywhere Podcasts are found.
+            </span>
         </div>
     );
 }
