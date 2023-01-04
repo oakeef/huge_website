c69c9bad8d81cb50523541f7790b3335cd1f52e7
Kortney <kortney@iMac.local>
2023-01-04 14:26:36 -0400

    moved xml parser and helper functions to get podcast eps from rss feed out of Home and up to App level so that it can be passed to Episode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./Pages/Home/Home";
@@ -11,9 +11,90 @@ import HugeAss from "./Pages/HugeAss/HugeAss";
 import Footer from "./Components/Footer/Footer";
 import Episodes from "./Pages/Episodes/Episodes";
 import Playback from "./Components/Playback/Playback";
+import axios from "axios";
+
+var XMLParser = require("react-xml-parser");
+
+function convertUnicode(myString) {
+    let convertedString = myString.replaceAll("%20", " ");
+    if (convertedString.length > 55) {
+        return convertedString.slice(0, 54) + "...";
+    } else return convertedString;
+}
+
+function stringChoppy(mystring) {
+    let slicedString = mystring.slice(0, mystring.length - 2);
+
+    return slicedString;
+}
+
+function convertDate(date) {
+    let dateArray = date.split(" ");
+    let end = "";
+    let day = "";
+    let digitOne = Number(dateArray[1][0]);
+    let digitTwo = Number(dateArray[1][1]);
+
+    digitOne === 0 ? (day = digitTwo) : (day = Number(dateArray[1]));
+
+    if (day === 1 || day === 21) {
+        end = "st";
+    } else if (day === 2 || day === 22) {
+        end = "nd";
+    } else if (day === 3 || day === 23) {
+        end = "rd";
+    } else end = "th";
+
+    return `${dateArray[2]} ` + `${day + end}, ` + dateArray[3];
+}
+
+function episodeCategory(title) {
+    let category = "";
+    let titleArray = title.split(" ");
+
+    if (titleArray[1] === "BBC") {
+        category = "Huge BBC";
+    } else if (titleArray[1] === "Last") {
+        category = "The Last Of Us Play The Last Of Us";
+    } else if (titleArray[0] === "TLOU") {
+        category = "The Last Of Us Part II";
+    } else category = "HUGE Podcast";
+    return category;
+}
 
 export default function App() {
     const [selectedEpisode, setSelectedEpisode] = useState(null);
+    const [podcastEpisodes, setPodcastEpisodes] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://feeds.libsyn.com/447303/rss", {
+                "Content-Type": "application/xml; charset=utf-8",
+            })
+            .then((response) => {
+                let episodes = [];
+                let xml = new XMLParser().parseFromString(response.data);
+
+                xml.children[0].children.forEach((episode, index) => {
+                    if (index >= 22) {
+                        let parsedEpisode = {
+                            title: convertUnicode(episode.children[0].value),
+                            subtitle: stringChoppy(episode.children[12].value),
+                            category: episodeCategory(
+                                episode.children[0].value
+                            ),
+                            image: episode.children[5].attributes.href,
+                            date: convertDate(episode.children[2].value),
+                            link: stringChoppy(
+                                episode.children[8].attributes.url
+                            ),
+                        };
+                        episodes.push(parsedEpisode);
+                    }
+                });
+                setPodcastEpisodes(episodes);
+            });
+    }, []);
 
     return (
         <>
@@ -23,7 +104,10 @@ export default function App() {
                     <Route
                         index
                         element={
-                            <Home setSelectedEpisode={setSelectedEpisode} />
+                            <Home
+                                setSelectedEpisode={setSelectedEpisode}
+                                podcastEpisodes={podcastEpisodes}
+                            />
                         }
                     />
                     <Route path="About" element={<About />} />
