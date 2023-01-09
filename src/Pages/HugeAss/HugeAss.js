b1ccaef8fa6e7f6f916f2a62f2e14b0977c4febc
Kortney <kortney@iMac.local>
2023-01-09 16:29:43 -0400

    played around with css to get paragraphs and embed video where I wanted it

--- src/Pages/HugeAss/HugeAss.js
+++ src/Pages/HugeAss/HugeAss.js
@@ -4,23 +4,24 @@ import "./HugeAss.css";
 export default function HugeAss() {
     return (
         <div className="hugeAssContainer">
-            <p>Huge Ass</p>
-            <div className="projectDescription">
-                <p>
-                    HUGE ASS was spawned from the covid-19 pandemic. The HUGE
-                    Crew wanted to make something together but also remote, so
-                    we decided to do some asynchronous speed running. Each
-                    player spends 30 mins playing a particular game and records
-                    it. We then sync up the footage and do some fun commentary
-                    over top and see who was faster. Eventually, finding out who
-                    the HUGEst ASS truely is.
-                </p>
+            <div className="hugeAssIntroContainer">
+                <div className="projectDescription">
+                    <p>
+                        HUGE ASS was spawned from the covid-19 pandemic. The
+                        HUGE Crew wanted to make something together but also
+                        remote, so we decided to do some asynchronous speed
+                        running. Each player spends 30 mins playing a particular
+                        game and records it. We then sync up the footage and do
+                        some fun commentary over top and see who was faster.
+                        Eventually, finding out who the HUGEst ASS truely is.
+                    </p>
 
-                <p>
-                    You can check out Season One on the HUGE Youtube Channel.
-                    Season Two is availble by{" "}
-                    <span>subscribing to Patreon.</span>
-                </p>
+                    <p>
+                        You can check out Season One on the HUGE Youtube
+                        Channel. Season Two is availble by{" "}
+                        <span>subscribing to Patreon.</span>
+                    </p>
+                </div>
                 <iframe
                     width="450"
                     height="315"
@@ -28,7 +29,6 @@ export default function HugeAss() {
                     src="https://www.youtube.com/embed/OBq-CJizT9w"
                 ></iframe>
             </div>
-            <h3>Season One</h3>
         </div>
     );
 }
