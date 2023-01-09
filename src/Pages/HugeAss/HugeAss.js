5b4813b1575fd014372b0221aea3ce9a22317e0f
Kortney <kortney@iMac.local>
2023-01-09 15:57:21 -0400

    added description for HugeAss

--- src/Pages/HugeAss/HugeAss.js
+++ src/Pages/HugeAss/HugeAss.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "./HugeAss.css";
 
 export default function HugeAss() {
     return (
-        <>
+        <div className="hugeAssContainer">
             <p>Huge Ass</p>
-        </>
+            <div>
+                <p>
+                    HUGE ASS was spawned from the covid-19 pandemic. The HUGE
+                    Crew wanted to make something together but also remote, so
+                    we decided to do some asynchronous speed running. Each
+                    player spends 30 mins playing a particular game and records
+                    it. We then sync up the footage and do some fun commentary
+                    over top and see who was faster. Eventually, finding out who
+                    the HUGEst ASS truely is.
+                </p>
+
+                <p> You can check it out on the HUGE Youtube Channel.</p>
+            </div>
+        </div>
     );
 }
