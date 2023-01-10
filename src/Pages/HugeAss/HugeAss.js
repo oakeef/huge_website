9dd80f65c5d449c6f0507974978800a9933a5694
Kortney <kortney@iMac.local>
2023-01-10 16:01:48 -0400

    fixed header image sizing by adding media query and reorganizing divs

--- src/Pages/HugeAss/HugeAss.js
+++ src/Pages/HugeAss/HugeAss.js
@@ -9,96 +9,99 @@ import patreonImg from "../../images/PatreonLogoName.png";
 
 export default function HugeAss() {
     return (
-        <div className="hugeAssContainer">
+        <div className="outerContainer">
             <img
                 className="projectHeader"
                 src={hugeassPic}
                 alt="beerGoggles logo"
             />
-            <div className="hugeAssIntroContainer">
-                <div className="projectDescription">
-                    <p>
-                        HUGE ASS was spawned from the covid-19 pandemic. The
-                        HUGE Crew wanted to make something together but also
-                        remote, so we decided to do some asynchronous speed
-                        running. Each player spends 30 mins playing a particular
-                        game and records it. We then sync up the footage and do
-                        some fun commentary over top and see who was faster.
-                        Eventually, finding out who the HUGEst ASS truely is.
-                    </p>
+            <div className="hugeAssContainer">
+                <div className="hugeAssIntroContainer">
+                    <div className="projectDescription">
+                        <p>
+                            HUGE ASS was spawned from the covid-19 pandemic. The
+                            HUGE Crew wanted to make something together but also
+                            remote, so we decided to do some asynchronous speed
+                            running. Each player spends 30 mins playing a
+                            particular game and records it. We then sync up the
+                            footage and do some fun commentary over top and see
+                            who was faster. Eventually, finding out who the
+                            HUGEst ASS truely is.
+                        </p>
 
-                    <p>
-                        You can check out Season One on the HUGE Youtube
-                        Channel. Season Two is availble by {""}
-                        <span className="hugeAssBold">
-                            <a
-                                href="https://www.patreon.com/hoppedupeast"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                subscribing to Patreon.
-                            </a>
-                        </span>
-                    </p>
-                    <div className="beerGogglesListenOnContainer">
-                        <h3 className="listenOnIconAndTitle">
+                        <p>
+                            You can check out Season One on the HUGE Youtube
+                            Channel. Season Two is availble by {""}
+                            <span className="hugeAssBold">
+                                <a
+                                    href="https://www.patreon.com/hoppedupeast"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    subscribing to Patreon.
+                                </a>
+                            </span>
+                        </p>
+                        <div className="beerGogglesListenOnContainer">
+                            <h3 className="listenOnIconAndTitle">
+                                <img
+                                    className="podIcon"
+                                    src={podIcon}
+                                    alt="podcatcher icon"
+                                />
+                                Subscribe On:
+                            </h3>
                             <img
-                                className="podIcon"
-                                src={podIcon}
-                                alt="podcatcher icon"
+                                className="youtubeIcon"
+                                src={youtubeIcon}
+                                alt="youtube triangle icon"
                             />
-                            Subscribe On:
-                        </h3>
-                        <img
-                            className="youtubeIcon"
-                            src={youtubeIcon}
-                            alt="youtube triangle icon"
-                        />
-                        <span className="listenOnChannel">
+                            <span className="listenOnChannel">
+                                <a
+                                    href="https://www.youtube.com/@HoppedUpGamingEast/featured"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Youtube
+                                </a>
+                            </span>
+                        </div>
+
+                        <div className="listenOnDiscord">
+                            <img
+                                className="logo"
+                                src={discordIcon}
+                                alt="discord logo"
+                            />
                             <a
-                                href="https://www.youtube.com/@HoppedUpGamingEast/featured"
+                                href="https://bit.ly/hoppedupdiscord"
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >
-                                Youtube
+                                <p className="listenOnDiscordText">
+                                    Discord Server
+                                </p>
                             </a>
-                        </span>
+                        </div>
                     </div>
-
-                    <div className="listenOnDiscord">
-                        <img
-                            className="logo"
-                            src={discordIcon}
-                            alt="discord logo"
-                        />
-                        <a
-                            href="https://bit.ly/hoppedupdiscord"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <p className="listenOnDiscordText">
-                                Discord Server
-                            </p>
-                        </a>
-                    </div>
+                    <figure>
+                        <iframe
+                            width="450"
+                            height="315"
+                            title="YouTubeEmbed HUGE ASS Playlist"
+                            src="https://www.youtube.com/embed/videoseries?list=PLXnsyGNMKFBgo8lYTARS244KlTaO1wTXb"
+                        ></iframe>
+                        <figcaption className="hugeAssfigCapt">
+                            Season One Playlist
+                        </figcaption>
+                    </figure>
                 </div>
-                <figure>
-                    <iframe
-                        width="450"
-                        height="315"
-                        title="YouTubeEmbed HUGE ASS Playlist"
-                        src="https://www.youtube.com/embed/videoseries?list=PLXnsyGNMKFBgo8lYTARS244KlTaO1wTXb"
-                    ></iframe>
-                    <figcaption className="hugeAssfigCapt">
-                        Season One Playlist
-                    </figcaption>
-                </figure>
+                <Patreon
+                    patreonTitle="Get Season Two Now on Patreon"
+                    patreonText="For early access to HUGE A-Synchronous Speedrunning Season 2, click the link below to check out our support options. Your support is super appreciated and allows up to keep doing what we love!"
+                    patreonimage={patreonImg}
+                />
             </div>
-            <Patreon
-                patreonTitle="Get Season Two Now on Patreon"
-                patreonText="For early access to HUGE A-Synchronous Speedrunning Season 2, click the link below to check out our support options. Your support is super appreciated and allows up to keep doing what we love!"
-                patreonimage={patreonImg}
-            />
         </div>
     );
 }
--- src/Pages/Xcommittee/Xcommittee.js
+++ src/Pages/Xcommittee/Xcommittee.js
@@ -54,102 +54,105 @@ export default function Xcommittee() {
     }, []);
 
     return (
-        <div className="xcomContainer">
+        <div className="outerContainer">
             <img
                 className="projectHeader"
                 src={XcommitteePic}
                 alt="xcombanner"
             />
+            <div className="xcomContainer">
+                <div className="xcomIntroContainer">
+                    <div className="projectDescription">
+                        <p>
+                            An interesting take on playing the strategy game
+                            XCOM. We have one save file and pass it around for 1
+                            hour turns and meet up at the end of each round and
+                            talk about what happened. Basically playing XCOM by
+                            commitee.
+                        </p>
+                        <p>
+                            Season 1 covers the events of XCOM Enemy Unkown and
+                            Enemy Within Expansion. Season 2 covers the events
+                            of XCOM 2, Alien Hunters and War of the Chosen
+                            Expansion. You can find both seasons on Apple
+                            Podcasts, Google Podcasts or anywhere Podcasts are
+                            found.
+                        </p>
+                    </div>
 
-            <div className="xcomIntroContainer">
-                <div className="projectDescription">
-                    <p>
-                        An interesting take on playing the strategy game XCOM.
-                        We have one save file and pass it around for 1 hour
-                        turns and meet up at the end of each round and talk
-                        about what happened. Basically playing XCOM by commitee.
-                    </p>
-                    <p>
-                        Season 1 covers the events of XCOM Enemy Unkown and
-                        Enemy Within Expansion. Season 2 covers the events of
-                        XCOM 2, Alien Hunters and War of the Chosen Expansion.
-                        You can find both seasons on Apple Podcasts, Google
-                        Podcasts or anywhere Podcasts are found.
-                    </p>
+                    <iframe
+                        width="450"
+                        height="315"
+                        title="YouTubeEmbed"
+                        src="https://www.youtube.com/embed/41g1NHMjw3M"
+                    ></iframe>
                 </div>
+                <ListenOn
+                    offsetValue={"-50px"}
+                    googleLink={
+                        "https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy5saWJzeW4uY29tLzQ0ODE4OC9yc3M"
+                    }
+                    appleLink={
+                        "https://podcasts.apple.com/us/podcast/x-committee/id979571952"
+                    }
+                    spotifyLink={
+                        "https://open.spotify.com/show/1z0C7QHykxMdrCQNxqqKCw"
+                    }
+                />
 
-                <iframe
-                    width="450"
-                    height="315"
-                    title="YouTubeEmbed"
-                    src="https://www.youtube.com/embed/41g1NHMjw3M"
-                ></iframe>
+                <h3 className="xcomSeasonHeader">Season One</h3>
+
+                {xcomOneEps.length > 0 && (
+                    <ReactSimplyCarousel
+                        className="xcomEpisodesContainer"
+                        activeSlideIndex={activeSlideIndex}
+                        onRequestChange={setActiveSlideIndex}
+                        itemsToShow={3}
+                        itemsToScroll={1}
+                        infinite={false}
+                        speed={60}
+                        backwardBtnProps={{
+                            className: "latestEpButton",
+                            children: "←",
+                        }}
+                        forwardBtnProps={{
+                            className: "latestEpButton",
+                            children: "→",
+                        }}
+                    >
+                        {xcomOneEps.map((episode, i) => {
+                            return <Episode key={i} episode={episode} />;
+                        })}
+                    </ReactSimplyCarousel>
+                )}
+
+                <h3 className="xcomSeasonHeader">Season Two</h3>
+
+                {xcomTwoEps.length > 0 && (
+                    <ReactSimplyCarousel
+                        className="xcomEpisodesContainerTwo"
+                        activeSlideIndex={activeSlideIndexB}
+                        onRequestChange={setActiveSlideIndexB}
+                        itemsToShow={3}
+                        draggable={true}
+                        itemsToScroll={1}
+                        infinite={false}
+                        speed={60}
+                        backwardBtnProps={{
+                            className: "latestEpButton",
+                            children: "←",
+                        }}
+                        forwardBtnProps={{
+                            className: "latestEpButton",
+                            children: "→",
+                        }}
+                    >
+                        {xcomTwoEps.map((episode, i) => {
+                            return <Episode key={i} episode={episode} />;
+                        })}
+                    </ReactSimplyCarousel>
+                )}
             </div>
-            <ListenOn
-                offsetValue={"-50px"}
-                googleLink={
-                    "https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy5saWJzeW4uY29tLzQ0ODE4OC9yc3M"
-                }
-                appleLink={
-                    "https://podcasts.apple.com/us/podcast/x-committee/id979571952"
-                }
-                spotifyLink={
-                    "https://open.spotify.com/show/1z0C7QHykxMdrCQNxqqKCw"
-                }
-            />
-
-            <h3 className="xcomSeasonHeader">Season One</h3>
-
-            {xcomOneEps.length > 0 && (
-                <ReactSimplyCarousel
-                    className="xcomEpisodesContainer"
-                    activeSlideIndex={activeSlideIndex}
-                    onRequestChange={setActiveSlideIndex}
-                    itemsToShow={3}
-                    itemsToScroll={1}
-                    infinite={false}
-                    speed={60}
-                    backwardBtnProps={{
-                        className: "latestEpButton",
-                        children: "←",
-                    }}
-                    forwardBtnProps={{
-                        className: "latestEpButton",
-                        children: "→",
-                    }}
-                >
-                    {xcomOneEps.map((episode, i) => {
-                        return <Episode key={i} episode={episode} />;
-                    })}
-                </ReactSimplyCarousel>
-            )}
-
-            <h3 className="xcomSeasonHeader">Season Two</h3>
-
-            {xcomTwoEps.length > 0 && (
-                <ReactSimplyCarousel
-                    className="xcomEpisodesContainerTwo"
-                    activeSlideIndex={activeSlideIndexB}
-                    onRequestChange={setActiveSlideIndexB}
-                    itemsToShow={3}
-                    draggable={true}
-                    itemsToScroll={1}
-                    infinite={false}
-                    speed={60}
-                    backwardBtnProps={{
-                        className: "latestEpButton",
-                        children: "←",
-                    }}
-                    forwardBtnProps={{
-                        className: "latestEpButton",
-                        children: "→",
-                    }}
-                >
-                    {xcomTwoEps.map((episode, i) => {
-                        return <Episode key={i} episode={episode} />;
-                    })}
-                </ReactSimplyCarousel>
-            )}
         </div>
     );
 }
