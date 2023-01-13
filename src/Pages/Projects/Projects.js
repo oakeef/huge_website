264ccffbe3fae28dbdfd61be766ed1db39a091a8
Kortney <kortney@iMac.local>
2023-01-13 10:40:20 -0400

    tried out picture in Contact and removed it, added TODO in Projects

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -3,7 +3,6 @@ import HugeProject from "../../Components/HugeProject/HugeProject";
 import XcommitteePic from "../../images/xcommittee.jpg";
 import BeerGoggles from "../../images/BeerGoggles.png";
 import hugeassPic from "../../images/hugeass.jpg";
-
 import "./Projects.css";
 
 export default function Projects() {
@@ -35,6 +34,7 @@ export default function Projects() {
                 "Other HUGE Projects including The Last of Us Play The Last Of Us, HUGE Boys Book Club as well as few 12 and 24 hour charity livestreams in support of the IWK Children's Hospital and Extra Life.",
             link: "/OtherProjects",
             photo: XcommitteePic,
+            //placeholder - TODO:replsce with an "other projects image"
         },
     ];
 
