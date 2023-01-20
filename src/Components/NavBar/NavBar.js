09528c6c42a520b10cae56f6496b96858acb8118
Kortney <kortney@iMac.local>
2023-01-20 15:08:53 -0400

    started making hamburger nav

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -7,48 +7,52 @@ import patreon from "../../images/Patreon.png";
 export default function NavBar() {
     return (
         <div className="navContainer">
-            <ul className="navLinks">
-                <li className="navLinkOuter">
-                    <Link className="navLink" to="/">
-                        Home
-                    </Link>
-                </li>
-                <li className="navLinkOuter">
-                    <Link className="navLink" to="/About">
-                        About
-                    </Link>
-                </li>
-                <li className="navLinkOuter">
-                    <Link className="navLink" to="/Episodes">
-                        Episodes
-                    </Link>
-                </li>
-                <li className="navLinkOuter">
-                    <Link className="navLink" to="/Projects">
-                        Projects
-                    </Link>
-                </li>
+            <div className="navMainContainer">
+                <ul className="navLinks">
+                    <li className="navLinkOuter">
+                        <Link className="navLink" to="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="navLinkOuter">
+                        <Link className="navLink" to="/About">
+                            About
+                        </Link>
+                    </li>
+                    <li className="navLinkOuter">
+                        <Link className="navLink" to="/Episodes">
+                            Episodes
+                        </Link>
+                    </li>
+                    <li className="navLinkOuter">
+                        <Link className="navLink" to="/Projects">
+                            Projects
+                        </Link>
+                    </li>
 
-                <li className="navLinkOuter">
-                    <Link className="navLink" to="/Contact">
-                        Contact
-                    </Link>
-                </li>
-            </ul>
-            <a
-                href="https://www.patreon.com/hoppedupeast"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <button className="patreonDonate">
-                    <img
-                        className="navPatreonLogo"
-                        src={patreon}
-                        alt="Patreon P icon"
-                    />
-                    <span>DONATE</span>
-                </button>
-            </a>
+                    <li className="navLinkOuter">
+                        <Link className="navLink" to="/Contact">
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+                <a
+                    href="https://www.patreon.com/hoppedupeast"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <button className="patreonDonate">
+                        <img
+                            className="navPatreonLogo"
+                            src={patreon}
+                            alt="Patreon P icon"
+                        />
+                        <span>DONATE</span>
+                    </button>
+                </a>
+            </div>
+
+            <ul className="navHamburger"></ul>
         </div>
     );
 }
