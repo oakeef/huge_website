c4d973861057d8a91df64ea54053dc91b12f088f
Kortney <kortney@iMac.citywide.eastlink.ca>
2023-01-27 16:44:56 -0400

    rewrote hero section so hero image would fit screen regardless of device screen size

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -8,34 +8,36 @@ export default function Hero(props) {
     const { episode, setSelectedEpisode } = props;
 
     return (
-        <div className="heroContainer">
-            <div className="heroText">
-                <img
-                    className="heroImageMediaQ"
-                    src={hugeCrewImg}
-                    alt="HUGE Logo"
-                />
-                <h2 className="heroMainText">Hopped-Up Gaming: East </h2>
+        <div className="outerContainer">
+            <img
+                className="heroImageMediaQ"
+                src={hugeCrewImg}
+                alt="HUGE Logo"
+            />
+            <div className="heroContainer">
+                <div className="heroText">
+                    <h2 className="heroMainText">Hopped-Up Gaming: East </h2>
 
-                <Featured
-                    episode={episode}
-                    setSelectedEpisode={setSelectedEpisode}
-                />
-                <ListenOn
-                    offsetValue={"10px"}
-                    googleLink={
-                        "https://podcasts.google.com/feed/aHR0cHM6Ly9ob3BwZWR1cGVhc3QuaGlwY2FzdC5jb20vcnNzL2hvcHBlZHVwZ2FtaW5nZWFzdC54bWw"
-                    }
-                    appleLink={
-                        "https://podcasts.apple.com/us/podcast/hopped-up-gaming-east/id797058638"
-                    }
-                    spotifyLink={
-                        "https://open.spotify.com/show/2dTRwdB4bHYsCI2CN9OV2m"
-                    }
-                />
+                    <Featured
+                        episode={episode}
+                        setSelectedEpisode={setSelectedEpisode}
+                    />
+                    <ListenOn
+                        offsetValue={"10px"}
+                        googleLink={
+                            "https://podcasts.google.com/feed/aHR0cHM6Ly9ob3BwZWR1cGVhc3QuaGlwY2FzdC5jb20vcnNzL2hvcHBlZHVwZ2FtaW5nZWFzdC54bWw"
+                        }
+                        appleLink={
+                            "https://podcasts.apple.com/us/podcast/hopped-up-gaming-east/id797058638"
+                        }
+                        spotifyLink={
+                            "https://open.spotify.com/show/2dTRwdB4bHYsCI2CN9OV2m"
+                        }
+                    />
+                </div>
+
+                <img className="heroImage" src={hugeCrewImg} alt="HUGE Logo" />
             </div>
-
-            <img className="heroImage" src={hugeCrewImg} alt="HUGE Logo" />
         </div>
     );
 }
